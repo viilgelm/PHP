9f387b5d8c1564651deb6731cdf2c639a71b273c
unknown < @spk.local>
2022-04-01 10:30:55 +0300

    commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+# Подключение конфигурации
+require_once "config.php";
+# Подключение автозагрузчика
+require_once "autoload.php";
+
+# Подгрузка файла маршрутизации
+require_once "route.php";
